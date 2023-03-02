c8ede57c20ca98437ee0e4807fc663b7c95e248d
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-02 16:43:54 +0500

    posgress cods

---
+++ POSTGRESS.sql
@@ -0,0 +1,243 @@
+PSQL:
+PostgreSQL terminalining o'zi. Bunda, PostgreSQL ma'lumotlar bazasi bilan ishlash uchun xizmat qiluvchi interfeys mavjud.
+
+CREATE
+DATABASE:
+Yangi ma'lumotlar bazasini yaratish uchun foydalaniladi.
+
+DROP DATABASE: Ma'lumotlar bazasini o'chirish uchun foydalaniladi.
+
+CREATE TABLE: Yangi jadvalni yaratish uchun foydalaniladi.
+
+ALTER TABLE: Jadvalni o'zgartirish uchun foydalaniladi.
+
+DROP
+TABLE:
+Jadvalni o'chirish uchun foydalaniladi.
+
+INSERT INTO: Jadvalga ma'lumot qo'shish uchun foydalaniladi.
+
+SELECT: Ma'lumotlar bazasidan ma'lumot olish uchun foydalaniladi.
+
+UPDATE: Jadvaldagi ma'lumotlarni yangilash uchun foydalaniladi.
+
+DELETE
+FROM:
+Jadvaldan ma'lumot o'chirish uchun foydalaniladi.
+
+WHERE:
+SELECT, 
+UPDATE, va
+DELETE FROM so
+'rovlarida ma'lumotlarni qidirish uchun shartlar belgilash uchun foydalaniladi.
+
+LIKE:
+SELECT, 
+UPDATE, va
+DELETE FROM so
+'rovlarida ma'lumotlarni qidirish uchun belgi orqali qidirish uchun foydalaniladi.
+
+ORDER
+BY:
+SELECT so'roviga natijalarni tartiblash uchun foydalaniladi.
+
+GROUP BY: SELECT so'
+roviga natijalarni guruhlashing uchun foydalaniladi.
+
+JOIN:
+Bitta jadvaldan boshqa jadvallarga bog'lanish uchun foydalaniladi.
+
+INNER JOIN: Bitta jadvaldan boshqa jadvalga ichki birlashma qilish uchun foydalaniladi.
+
+LEFT JOIN: Bitta jadvaldan boshqa jadvalga chapa birlashma qilish uchun foydalaniladi.
+
+RIGHT JOIN: Bitta jadvaldan boshqa jadvalga o'ng birlashma qilish uchun foydalaniladi.
+
+FULL OUTER
+JOIN:
+Bitta jadvaldan boshqa jadvalga to'liq birlashma qilish uchun foydalaniladi.
+
+UNION: Bitta yoki undan ko'p
+SELECT so'rovlari natijalarini birlashtirish uchun foydalaniladi.
+
+COUNT: Ma'
+lumotlar bazasid
+
+AVG:
+Ma'lumotlar bazasidagi sonlar yig'indisini o'rta arifmetigini hisoblash uchun foydalaniladi.
+
+SUM: Ma'lumotlar bazasidagi sonlar yig'indisini hisoblash uchun foydalaniladi.
+
+MAX: Ma'lumotlar bazasidagi eng yuqori qiymatni qaytarish uchun foydalaniladi.
+
+MIN:
+Ma'lumotlar bazasidagi eng past qiymatni qaytarish uchun foydalaniladi.
+
+CONSTRAINT: Jadvalda ma'lumotlarni cheklovchi shartlar yaratish uchun foydalaniladi.
+
+PRIMARY
+KEY:
+Jadvaldagi bitta yoki bir nechta ustunlarni kalit sifatida belgilash uchun foydalaniladi.
+
+FOREIGN
+KEY:
+Boshqa jadvaldagi ustunlarga qarab jadvalga bog'lash uchun foydalaniladi.
+
+INDEX: Jadvalga tezkorlik vaqti qisqartirish uchun ma'lumotlarni indekslash uchun foydalaniladi.
+
+VIEW:
+Ma'lumotlar bazasida mavjud ma'lumotlar bazasi obyektlari uchun ko'rib chiqish uchun foydalaniladi.
+
+TRUNCATE: Jadvaldagi barcha ma'lumotlarni o'chirish uchun foydalaniladi.
+
+COMMIT: Tranzaksiyalardagi ma'lumotlarni tasdiqlash uchun foydalaniladi.
+
+ROLLBACK:
+Tranzaksiyalardagi o'zgarishlarni bekor qilish uchun foydalaniladi.
+
+SAVEPOINT: Tranzaksiyalardagi joyni saqlash uchun foydalaniladi.
+
+SERIAL: Jadvalda yangi ma'lumot qo'shish uchun avtomatik raqam yaratish uchun foydalaniladi.
+
+SEQUENCE: Ma'lumotlar bazasidagi raqamlar ketma-ketligini yaratish uchun foydalaniladi.
+
+DISTINCT:
+SELECT so'roviga natijalarda takrorlanmasdan qiymatlarni qaytarish uchun foydalaniladi.
+
+CASE: Qidiruvlar uchun shartlarni belgilash uchun foydalaniladi.
+
+COALESCE: Null qiymatlarini boshqa qiymatlar bilan almashtirish uchun foydalaniladi.
+
+EXTRACT: Ma'
+lumotlar bazasidagi vaqtlarni olib chiqish uchun foydalaniladi.
+
+JOIN:
+Bitta yoki bir nechta jadvaldagi ma'lumotlarni bir-biri bilan bog'lash uchun foydalaniladi.
+
+UNION:
+Bitta yoki bir nechta jadvaldagi o'zaro mos keladigan ma'lumotlar to'plamlarini birlashtirish uchun foydalaniladi.
+
+INTERSECT: Bitta yoki bir nechta jadvaldagi o'zaro mos keladigan ma'lumotlar to'plamlarining kesishmasini topish uchun foydalaniladi.
+
+EXCEPT:
+Bitta jadvaldagi ma'lumotlardan boshqa jadvaldagi ma'lumotlar to'plamini chiqarish uchun foydalaniladi.
+
+GROUP BY: SELECT so'rovidagi ustunlarni guruhlarga bo'lish uchun foydalaniladi.
+
+HAVING: GROUP BY so'rovidan keyin guruhlarga qo'yilgan shartni bajarish uchun foydalaniladi.
+
+CASCADE: Cheklovlarni o'chirish, o'zgartirish, yaratish kabi operatsiyalarda cheklovga bog'liq obyektlarni o'chirish uchun foydalaniladi.
+
+INHERITS: Kalitlarni va jadval yoki obyektlardan xosil qilingan sifatlarini qabul qilish uchun foydalaniladi.
+
+GRANT: Foydalanuvchiga ma'lumotlar bazasidagi huquqlarni berish uchun foydalaniladi.
+
+REVOKE:
+Foydalanuvchiga ma'lumotlar bazasidagi huquqlarni bekor qilish uchun foydalaniladi.
+
+INDEX: Bir yoki bir nechta ustunlarga tezkor murojat uchun indeks yaratish uchun foydalaniladi.
+
+CONSTRAINT: Jadval ustunlariga cheklov qo'shish uchun foydalaniladi.
+
+UNIQUE:
+Jadval ustunlaridagi ma'lumotlar uchun bir xil qiymatlarni qabul qilish uchun cheklov qo'shish uchun foydalaniladi.
+
+PRIMARY
+KEY:
+Jadval ustunlaridagi ma'lumotlarni aniqlash uchun yagona cheklovni yaratish uchun foydalaniladi.
+
+FOREIGN KEY: Boshqa jadvaldagi ustunlarga bog'liq ma'lumotlar uchun cheklov qo'shish uchun foydalaniladi.
+
+CHECK:
+Jadval ustunlaridagi ma'lumotlar uchun shartlarni aniqlash uchun foydalaniladi.
+
+DEFAULT: Jadval ustunlarida qiymatlar aniqlash uchun standart qiymat berish uchun foydalaniladi.
+
+ALTER: Ma'lumotlar bazasidagi obyektlarni o'zgartirish uchun foydalaniladi.
+
+DROP: Ma'lumotlar bazasidagi obyektlarni o'chirish uchun foydalaniladi.
+
+COMMIT: Tranzaktsiyani amalga oshirish va ma'lumotlar bazasidagi o'zgarishlarni saqlash uchun foydalaniladi.
+ROLLBACK: Tranzaktsiyani bekor qilish va oldingi holatiga qaytish uchun foydalaniladi.
+
+SAVEPOINT: Tranzaktsiyalarning o'rtasida barcha yoki bir nechta operatsiyalarni bajarish uchun saqlangan nuqtaning belgilash uchun foydalaniladi.
+
+GRANT:
+Ma'lumotlar bazasidagi obyektlarga foydalanuvchilarga ruxsat berish uchun foydalaniladi.
+
+REVOKE: Foydalanuvchilarning ma'lumotlar bazasidagi obyektlarga kirish ruxsatini bekor qilish uchun foydalaniladi.
+
+ANALYZE:
+Ma'lumotlar bazasidagi jadval va indekslarni tekshirib, statistik ma'lumotlarni yangilash uchun foydalaniladi.
+
+VACUUM:
+Ma'lumotlar bazasidagi bo'sh joylarni o'chirib tashlash uchun foydalaniladi.
+
+CLUSTER: Jadvalni indeks asosida tartiblash uchun foydalaniladi.
+
+CREATE DATABASE: Yangi ma'lumotlar bazasi yaratish uchun foydalaniladi.
+
+CREATE
+USER:
+Yangi foydalanuvchi yaratish uchun foydalaniladi.
+
+ALTER
+USER:
+Foydalanuvchi haqidagi ma'lumotlarni o'zgartirish uchun foydalaniladi.
+
+DROP
+USER:
+Foydalanuvchini o'chirish uchun foydalaniladi.
+
+CREATE SCHEMA: Skhema yaratish uchun foydalaniladi.
+
+ALTER SCHEMA: Skhema haqidagi ma'lumotlarni o'zgartirish uchun foydalaniladi.
+
+DROP SCHEMA: Skhemani o'chirish uchun foydalaniladi.
+
+CREATE
+TABLESPACE:
+Jadvallarni saqlash uchun bo'sh joy yaratish uchun foydalaniladi.
+
+DROP TABLESPACE: Bo'sh joylarni o'chirish uchun foydalaniladi.
+
+CREATE EXTENSION: Ma'lumotlar bazasidagi funksiyalar va yorliqlarni yuklash uchun foydalaniladi.
+
+DROP
+EXTENSION:
+Ma'lumotlar bazasidagi funksiyalar va yorliqlarni o'chirish uchun foydalaniladi.
+
+CREATE
+TRIGGER:
+Jadval ustunlari uchun trigger yaratish uchun foydalaniladi.
+
+ALTER
+TRIGGER:
+Trigger haqidagi ma'lumotlarni o'zgartirish uchun foydalaniladi.
+
+DROP
+TRIGGER:
+Triggerni o'chirish uchun foydalaniladi.
+
+COMMENT: Ma'lumotlar bazasidagi obyektga izoh qo'shish uchun foydalaniladi.
+
+CREATE INDEX: Jadval ustunlariga indeks yaratish uchun foydalaniladi.
+
+DROP INDEX: Jadval ustunlaridagi indekslarni o'chirish uchun foydalaniladi.
+
+SET
+CONSTRAINTS:
+Tranzaktsiya belgilangan cheklovlarni yoqish yoki o'chirish uchun foydalaniladi.
+
+DISCARD: Ba'zi ma'lumotlar bazasi joylaridan olib tashlangan xatoliklar uchun foydalaniladi.
+
+RESET: Ma'lumotlar bazasidagi konfiquratsiyalar uchun asosiy qiymatlarga qaytarish uchun foydalaniladi.
+
+NOTIFY:
+Foydalanuvchilarga asinxron habarnoma yuborish uchun foydalaniladi.
+
+LISTEN:
+Asinxron habarnomalarni eshitish uchun foydalaniladi.
+
+UNLISTEN:
+Asinxron habarnomalarni eshitishni to'xtatish uchun foydalaniladi.
